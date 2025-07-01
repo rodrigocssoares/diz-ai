b75385da13e251d548ae5a554a32c238d14f68f0
rodrigocssoares <rod1234@gmail.com>
2025-07-01 17:55:28 -0300

    Ajusta vite.config.js para build no Vercel

--- Downloads/diz-ai/vite.config.js
+++ Downloads/diz-ai/vite.config.js
@@ -1,13 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: '.',
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: path.resolve(__dirname, 'index.html')
-    }
-  }
-});
+})
